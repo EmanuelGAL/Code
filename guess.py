a68843b4c87a8794524934c81fed1392018e7fe4
EmanuelGAL <21galan@parmiters.herts.sch.uk>
2026-09-08 10:20:30 +0100

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,3 +1,6 @@
 names = ["PROFESSOR PLUM", "MRS WHITE", "MR GREEN", "MRS PEACOCK", "MISS SCARLET", "COLONEL MUSTUARD"]
 print(names)
 print(names[2])
+rooms = ["STUDY", "KITCHEN", "HALL", "CONSERVATORY", "LOUNGE", "BALLROOM", "DINING ROOM", "LIBRARY", "BILLIARD ROOM"]
+print(rooms)
+print(rooms[4])
